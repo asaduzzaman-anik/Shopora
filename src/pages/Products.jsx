28503d544959cf722e187c2c807a9d73e889d727
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-13 09:22:21 +0600

    Update: Added api response handler and error handler feature

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,23 +3,51 @@ import ProductCard from "../components/ProductCard";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  const url = "https://dummyjson.com/products";
 
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data.products))
-      .catch((error) => console.log(error));
-  }, []);
-
-  console.log(products);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Invalid URL to fetch data from");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data.products);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, [url]);
 
   return (
     <div>
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      {error && (
+        <div className="container mx-auto h-screen flex justify-center items-center text-7xl font-bold">
+          {error}
+        </div>
+      )}
+
+      {isPending && (
+        <div className="container mx-auto h-screen flex justify-center items-center text-7xl font-bold">
+          Loading Items...
+        </div>
+      )}
+
+      {products && (
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
