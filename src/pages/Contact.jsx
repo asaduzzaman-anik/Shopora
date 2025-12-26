4c5b86cc2fe6a81b4f0c06b8da70ada231fed655
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-26 14:38:41 +0600

    Update: Update the Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,23 @@
 export default function Contact() {
   return (
-    <div className="max-w-7xl mx-auto px-2 h-screen">
-      <h1 className="text-3xl text-primary font-bold mb-3 sm:mb-6 text-center">
-        Contact Us
-      </h1>
-      <div className="bg-white rounded-2xl shadow shadow-secondary p-5 mt-5 sm:mt-20">
-        <h2 className="text-primary font-medium text-lg">Send us a message</h2>
-        <p className="text-gray-700 mb-8">
-          Have questions or feedback? Reach out to us using the form below or
-          send an email to{" "}
-          <span className="text-secondary font-semibold">
-            support@shopora.com
-          </span>
-          .
-        </p>
-
+    <div className="max-w-7xl mx-auto px-2 min-h-screen">
+      {/* Hero Section */}
+      <section className="bg-linear-to-r from-blue-600 to-purple-600 text-white rounded-2xl py-16 md:py-24">
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mb-6">Contact Us</h1>
+          <p className="text-xl md:text-2xl max-w-3xl mx-auto opacity-90">
+            Have questions or feedback? Reach out to us using the form below or
+            send an email to{" "}
+            <span className="text-gray-950 font-semibold">
+              support@shopora.com
+            </span>
+          </p>
+        </div>
+      </section>
+      <div className="bg-white ring ring-primary rounded-2xl p-5 my-5 sm:mt-20">
+        <h2 className="text-primary font-medium text-lg mb-5">
+          Send us a message
+        </h2>
         <form className="flex flex-col gap-4 max-w-md">
           <input
             type="text"
@@ -36,7 +39,7 @@ export default function Contact() {
           />
           <button
             type="submit"
-            className="bg-primary text-white px-6 py-3 rounded-xl hover:bg-secondary cursor-pointer transition"
+            className="bg-purple-700 text-white px-6 py-3 rounded-xl hover:bg-purple-500 cursor-pointer transition"
           >
             Send Message
           </button>
